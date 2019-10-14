b5dd213058e20d1597595936a5ecb3fdb8b4ec80
Mate <51403588+meetm473@users.noreply.github.com>
2019-10-14 22:55:30 +0530

    Add files via upload

---
+++ greenDetection.py
@@ -0,0 +1,68 @@
+#Tracking 1
+
+from imutils.video import VideoStream
+import argparse
+import cv2
+import imutils
+import time
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+# Grab the reference to the webcam
+vs = VideoStream(src=0).start()
+
+# allow the camera to warm up
+time.sleep(2.0)
+
+#keep looping
+while True:
+    #grab the current frame
+    frame = vs.read()
+
+    # resize the frame (aspect ratio is maintained), blur it, and convert it to the HSV.
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+    # find the largest contour in the mask, then use
+    # it to compute the minimum enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+    # only proceed if the radius meets a minimum size
+        if radius > 100:
+            print("GREEN Card detected!")
+        else:
+            print("Nothing detected.")
+	
+    # show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+vs.stop()
+# close all windows
+cv2.destroyAllWindows()
